1cfc8deb66db78ea7b0f04e6e92a65ad3bee704c
Carlos  chen <ccjjww1222@gmail.com>
2022-11-03 17:17:19 +0800

    1
    2

--- video_format/format_video.py
+++ video_format/format_video.py
@@ -4,12 +4,13 @@ import cv2
 import shutil
 import time
 
-EXTRACT_FREQUENCY = 1  # 帧提取频率
+EXTRACT_FREQUENCY = 4  # 帧提取频率
 
 def extract_frames(video_path, dst_folder, index):
     # 实例化视频对象
     video = cv2.VideoCapture(video_path)
     frame_count = 0
+    t0 = str(time.time())
     # 循环遍历视频中的所有帧
     while True:
         # 逐帧读取
@@ -19,29 +20,35 @@ def extract_frames(video_path, dst_folder, index):
         # 按照设置的频率保存图片
         if frame_count % EXTRACT_FREQUENCY == 0:
             # 设置保存文件名
-            save_path = "{}/{:>03d}.jpg".format(dst_folder, index)
+            save_path = "{}/{}-{:>03d}.jpg".format(dst_folder,t0,index)
             # 保存图片
             cv2.imwrite(save_path, frame)
             index += 1  # 保存图片数＋1
         frame_count += 1  # 读取视频帧数＋1
 
-    # 视频总帧数
-    print(f'the number of frames: {frame_count}')
+    # 视频总帧数˝
+    # print(f'the number of frames: {frame_count}')
     # 打印出所提取图片的总数
-    print("Totally save {:d} imgs".format(index - 1))
+    print("vide0:{}, image save {:d} imgs".format(video_path,index - 1))
     # 最后释放掉实例化的视频
     video.release()
+    return index
 
 
 def main(video_path):
     video_list = traversal_files(video_path)
+    totel_images = 0
     for item in video_list:
         image_path = os.path.join("./images", item)
         video_path = os.path.join("./video", item)
-        build_img_path(image_path)
-        # extract_frames(video_path,image_path,1)
-        print ("video {} done ".format(item))
-    pass
+        # build_img_path(image_path)
+        # images_number = extract_frames(video_path,image_path,1)
+        # print ("video {} done ".format(item))
+        # totel_images += images_number
+    print("Totally save {} imgs".format(totel_images))
+
+
+
 
 def build_img_path(path):
     try:
